c6ecbbcc576f34d2bdfedbdfa834f3fb958c81fd
Christian-Beninati <christianbeninati.info@gmail.com>
2023-09-01 22:07:17 +0100

    Added Show Projects

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('title', 'Project')
+
+@section('content')
+    <header>
+        <h1>{{ $project->title }}</h1>
+    </header>
+    <hr>
+
+    <div class="clearfix">
+        <img class="float-start me-2" src="{{ $project->image }}" alt="{{ $project->title }}">
+        <p>{{ $project->description }}</p>
+    </div>
+    <div>
+        <strong>Created on:</strong> {{ $project->created_at }}
+        <strong>Last edit:</strong> {{ $project->updated_at }}
+    </div>
+
+    <hr>
+    <footer class="d-flex justify-content-between">
+        <a href="{{ route('admin.projects.index') }}" class="btn btn-secondary">Back to list</a>
+        <div class="d-flex justify-content-end gap-2 ">
+            <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning">
+                <i class="fas fa-pencil me-2"></i> Edit
+            </a>
+            <form action="{{ route('admin.projects.destroy', $project) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger ">
+                    <i class="fas fa-trash me-2"></i>Delete
+                </button>
+            </form>
+        </div>
+    </footer>
+@endsection
