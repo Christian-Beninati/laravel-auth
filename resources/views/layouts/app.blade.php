043bd6b5b405b9237768fa426a878502cc0daa72
Christian-Beninati <christianbeninati.info@gmail.com>
2023-09-01 21:10:26 +0100

    Added Index Projects

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,14 @@
 
     <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
 
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    {{-- <!-- Fonts Awesome--> --}}
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+        integrity='sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=='
+        crossorigin='anonymous' />
 
     @yield('cdns')
 
