94ad39c35d4439abbb08dc109fd8ad43ec0fe595
Christian-Beninati <christianbeninati.info@gmail.com>
2023-09-02 18:12:11 +0100

    Added List of latest projects in Home (Guest)

--- app/Http/Controllers/Guest/HomeController.php
+++ app/Http/Controllers/Guest/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('guest.home');
+        $projects = Project::orderBy('created_at')->get();
+        return view('guest.home', compact('projects'));
     }
 }
