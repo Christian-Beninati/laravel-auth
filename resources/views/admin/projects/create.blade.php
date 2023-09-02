53f645e0f9e7855092ddbc6e86f0e27caec43c92
Christian-Beninati <christianbeninati.info@gmail.com>
2023-09-02 22:17:30 +0100

    Added image-preview.js

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -50,13 +50,5 @@
 @endsection
 
 @section('scripts')
-    <script>
-        const placeholder = 'https://marcolanci.it/utils/placeholder.jpg';
-        const imageField = document.getElementById('image');
-        const previewField = document.getElementById('image-preview');
-
-        imageField.addEventListener('input', () => {
-            previewField.src = imageField.value || placeholder;
-        })
-    </script>
+    @vite('resources/js/image-preview')
 @endsection
